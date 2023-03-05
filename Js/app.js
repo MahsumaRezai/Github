d07b79cbaf466d6b97b600ea7d568048637d66db
Your Name <you@example.com>
2023-03-04 21:54:33 -0800

    hello js code at my project

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+// select section 
+function select() {
+    document.getElementById("sel").style.width = "100px";
+    document.getElementById("sel").style.backgroundColor = "white";
+    document.getElementById("sel").style.color = "black";
+    document.getElementById("sel").style.gap = "5px";
+}
+select();
+// button section 
